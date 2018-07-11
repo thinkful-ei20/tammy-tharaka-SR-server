6c60038195fbd7a183d3475732ed567e9ccfa518
Tammy Tian <tiantammy@outlook.com>
2018-07-11 19:50:27 +0200

    updated data

--- users/models.js
+++ users/models.js
@@ -16,7 +16,7 @@ const UserSchema = mongoose.Schema({
   },
   firstName: {type: String, default: ''},
   lastName: {type: String, default: ''},
-  head:{type: String, default: ''},
+  head:{type: Object},
   questions:[{
     _id: {type: mongoose.Schema.Types.Object, ref: 'Question'},
     question:{type: String},
